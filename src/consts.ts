833b61afde770e68992ee6cddb711ef43fe2d93e
knnv-ar <106931704+knnv-ar@users.noreply.github.com>
2024-03-30 17:36:36 -0300

    introduccion is the home page [FIXED]

--- src/consts.ts
+++ src/consts.ts
@@ -37,18 +37,28 @@ export type Sidebar = Record<
 >
 export const SIDEBAR: Sidebar = {
   en: {
-    'Introducción': [
-      { text: 'Introduction', link: 'en/introduction' },
-      { text: 'Page 2', link: 'en/page-2' },
-      { text: 'Page 3', link: 'en/page-3' }
+    'General': [
+      { text: 'Introducción', link: 'es/introduccion' },
     ],
     'Taller de p5.js': [
-      { text: 'Clase 1. ¿Qué es p5.js?', link: 'en/que-es-p5js' },
-      { text: '1.1. Obtención/Instalación', link: 'en/obtencion-instalacion' },
-      { text: '1.2. Elementos del código', link: 'en/elementos-codigo' },
-      { text: '1.3 Dibujar', link: 'en/dibujar' },
-      { text: '1.4 Color', link: 'en/color' },
-    ]
+      { text: 'CLASE 1. ¿Qué es p5.js?', link: 'es/que-es-p5js' },
+      { text: '1.1. Obtención-Instalación', link: 'es/obtencion-instalacion' },
+      { text: '1.2. Elementos del código', link: 'es/elementos-codigo' },
+      { text: '1.3 Dibujar', link: 'es/dibujar' },
+      { text: '1.4 Color', link: 'es/color' },
+    ],
+    'ACSs': [
+      { text: 'ACS Nº 1', link: 'es/acs-1' },
+      { text: 'ACS Nº 2', link: 'es/acs-2' },
+      { text: 'ACS Nº 3', link: 'es/acs-3' },
+      { text: 'ACS Nº 4', link: 'es/acs-4' },
+      { text: 'ACS Nº 5', link: 'es/acs-5' },
+      { text: 'ACS Nº 6', link: 'es/acs-6' },
+    ],
+    'TPs': [
+      { text: 'TP Nº 1', link: 'es/tp-1' },
+      { text: 'TP Nº 2', link: 'es/tp-2' },
+    ],
   },
   es: {
     'General': [
@@ -73,6 +83,5 @@ export const SIDEBAR: Sidebar = {
       { text: 'TP Nº 1', link: 'es/tp-1' },
       { text: 'TP Nº 2', link: 'es/tp-2' },
     ],
-    
   }
 }
